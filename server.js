1df7ea7eee4edcde06737953f834b272475929a2
Zack Ward <zack@zadiastudio.com>
2017-01-09 22:30:34 -0700

    Correct natural date output, add tests

--- server.js
+++ server.js
@@ -4,28 +4,37 @@ var chrono = require('chrono-node');
 var app = express();
 var port = 3000;
 
+app.use('/static', express.static('public'));
+
 app.get('/', function (req, res) {
-    res.end("Syntax: ");
+    res.sendFile( __dirname + '/public/syntax.html', {}, function (error) {
+        if (error) {
+            res.status(500).send("Error");
+        }
+    });
 });
 
 app.get('/:dateString', function (req, res) {
     var parsedDate = chrono.parseDate(req.params.dateString, new Date());
     if (parsedDate) {
-        res.send({
-            "natural": parsedDate.toString(),
+        res.json({
+            "natural": moment(parsedDate).format("MMMM D, Y"),
             "unix": new Date(parsedDate).getTime() / 1000
         });
     } else if (moment.unix(req.params.dateString).isValid()) {
         // Ok, we're dealing with a unix Timestamp
         var unixDate = moment.unix(req.params.dateString);
-        res.send({
-            "natural": unixDate.toString(),
+        res.json({
+            "natural": unixDate.format("MMMM D, Y"),
             "unix": unixDate.unix()
         });
     } else {
-        res.send({"natural": null, "unix": null});
+        res.status(400).json({"natural": null, "unix": null});
     }
 });
 
-app.listen(port);
+app.listen(port, 'localhost');
 console.log("Timestamp Microservice listening on port " + port);
+
+// Make server available for Mocha testing
+module.exports = app;
--- test/test-server.js
+++ test/test-server.js
@@ -1,11 +1,142 @@
 var chai = require('chai');
+var expect = chai.expect;
 var chaiHttp = require('chai-http');
-var should = chai.should();
 var server = require('../server.js');
 
-describe('Server', function () {
-    // JSON object format: {'natural': "String", 'unix': number}
-    it('Should return a JSON object with the natural date and unix timestamp if given a natural language date');
-    it('Should return a JSON object with the natural date and unix timestamp if given accept a unix timestamp');
-    it('Should return a JSON object with null fields if given invalid input');
+chai.use(chaiHttp);
+
+describe('GET /', function () {
+    var res;
+
+    before(function (done) {
+        chai.request(server)
+            .get('/')
+            .end(function (err, response) {
+                if (err) {
+                    return done(err);
+                }
+                res = response;
+                done();
+            });
+    });
+
+    it('should return a 200 status', function (done) {
+                expect(res).to.have.status(200);
+                done();
+    });
+    it('should return an html page that displays the syntax');
+});
+
+describe('GET /Jan 1, 2010', function () {
+    var res;
+    before(function (done) {
+        chai.request(server)
+            .get('/Jan 1, 2010')
+            .end(function (err, response) {
+                if (err) {
+                    return done(err);
+                }
+                res = response;
+                done();
+            });
+    });
+
+    it('should return a 200 status', function (done) {
+        expect(res).to.have.status(200);
+        done();
+    });
+
+    it('should return a json object', function (done) {
+        expect(res).to.be.json;
+        done();
+    });
+
+    it('should have a natural field that is equal to "January 1, 2010"', function (done) {
+        expect(res.body).to.have.property('natural');
+        expect(res.body.natural).to.equal("January 1, 2010");
+        done();
+    });
+
+    it('should have a unix field that is between 1262303999 and 1262390400', function (done) {
+        expect(res.body).to.have.property('unix');
+        expect(res.body.unix).to.be.above(1262303999);
+        expect(res.body.unix).to.be.below(1262390400);
+        done();
+    });
+
+});
+
+
+describe('GET /1262347200', function () {
+    var res;
+    before(function (done) {
+        chai.request(server)
+            .get('/1262347200')
+            .end(function (err, response) {
+                if (err) {
+                    return done(err);
+                }
+                res = response;
+                done();
+            });
+    });
+
+    it('should return a 200 status', function (done) {
+        expect(res).to.have.status(200);
+        done();
+    });
+
+    it('should return a json object', function (done) {
+        expect(res).to.be.json;
+        done();
+    });
+
+    it('should have a natural field that is equal to "January 1, 2010"', function (done) {
+        expect(res.body).to.have.property('natural');
+        expect(res.body.natural).to.equal("January 1, 2010");
+        done();
+    });
+
+    it('should have a unix field that is between 1262303999 and 1262390400', function (done) {
+        expect(res.body).to.have.property('unix');
+        expect(res.body.unix).to.be.above(1262303999);
+        expect(res.body.unix).to.be.below(1262390400);
+        done();
+    });
+});
+
+describe('GET /Error state', function () {
+    var err;
+    var res;
+    before(function (done) {
+        chai.request(server)
+            .get('/Error state')
+            .end(function (error, response) {
+                err = error;
+                res = response;
+                done();
+            });
+    });
+    
+    it('should return a 400 status', function (done) {
+        expect(res).to.have.status(400);
+        done();
+    });
+
+    it('should return a json object', function (done) {
+        expect(res).to.be.json;
+        done();
+    });
+
+    it('should have a natural field with a value of null', function (done) {
+        expect(res.body).to.have.property('natural');
+        expect(res.body.natural).to.equal(null);
+        done();
+    });
+
+    it('should have a unix field with a value of null', function (done) {
+        expect(res.body).to.have.property('unix');
+        expect(res.body.unix).to.equal(null);
+        done();
+    });
 });
